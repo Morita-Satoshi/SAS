db952a613b9bbf7907df6a226f75f4543024f289
SATOSHI MORITA <e12313@kmjp.local>
2019-11-21 14:10:30 +0900

    postStudent API done.

--- src/postStudent.py
+++ src/postStudent.py
@@ -1,12 +1,24 @@
 import json
+import boto3 
 
 
-def getStudent(event, context):
+def postStudent(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
+    
+    item={
+        'studentId':event['studentId'],
+        'plantId':0,
+        'studentName':event['studentName']
+    }
+    dynamo = boto3.resource('dynamodb')
+    table = dynamo.Table('Asagao-manage')
+    table.put_item(Item=item)
+    
+    
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
