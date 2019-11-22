ec64ad95857a1d0e8862188a1f1f5e75fac2f97c
SATOSHI MORITA <e12313@kmjp.local>
2019-11-22 09:31:46 +0900

    adD

---
+++ screen_src/asagao/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img
+      alt="today"
+      src="https://asagao-automation.s3-ap-northeast-1.amazonaws.com/images/download.jpg"
+    />
+    <HelloWorld msg="今日の朝顔の様子です！" />
+    <APIcall
+      class="APIcall"
+      @loadStart="onLoadStart"
+      @loadComplete="onLoadComplete"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import APIcall from "./components/APICall.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      items: [],
+      loadProgress: false
+    };
+  },
+  method: {
+    onLoadStart() {
+      this.loadProgress = true;
+    },
+    onLoadComplete({ results }) {
+      this.items = results;
+      this.loadProgress = false;
+    }
+  },
+  components: {
+    HelloWorld,
+    APIcall
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ screen_src/asagao/src/components/APICall.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="APIcall_container">
+    <input class="text" type="text" v-model="qu" @keyup.enter="exe" />
+    <input class="submit" type="submit" value="APIcall" @click="exe" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      qu: ""
+    };
+  },
+  methods: {
+    async exe() {
+      this.$emit("loadStart");
+      const { data } = await axios.get(
+        `https://wvgpmj09ce.execute-api.ap-northeast-1.amazonaws.com/dev/students/morita/plants/${this.qu}`
+      );
+      this.$emit("loadDone", { results: data });
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+}
+h2 {
+  color: red;
+}
+p {
+  color: red;
+}
+</style>
